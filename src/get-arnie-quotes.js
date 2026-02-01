704b5d443ed5c9d9e109f0d472ff7f8bbf4ce1f5
AlbertAus <tonycdl80@gmail.com>
2026-02-01 16:05:55 +1100

    feat: add request and console.log the responses

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,15 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-
+  const requests = urls.map(async (url) => {
+    const response = await httpGet(url);
+    console.log('Response:', response);
+  });
+  
 };
 
+
+getArnieQuotes(['http://www.smokeballdev.com/arnie0', 'http://www.smokeballdev.com/arnie1', 'http://www.smokeballdev.com/arnie2']);
 module.exports = {
   getArnieQuotes,
 };
