2593f075a89ecb321b46305b2c9851fa8dcf34bf
AlbertAus <tonycdl80@gmail.com>
2026-02-01 16:02:56 +1100

    feat: remove unuse comments

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,7 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+
 };
 
 module.exports = {
