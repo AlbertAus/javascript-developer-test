ef9ef42741d4e353400c66983a584cde51b86dff
AlbertAus <tonycdl80@gmail.com>
2026-02-01 16:15:46 +1100

    feat: add logic and test the code

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,13 +3,24 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   const requests = urls.map(async (url) => {
     const response = await httpGet(url);
-    console.log('Response:', response);
+     console.log('Response:', response);
+    const { message } = JSON.parse(response.body);
+    
+    if (response.status === 200) {
+      return { 'Arnie Quote': message };
+    } else {
+      return { 'FAILURE': message };
+    }
   });
   
+  return Promise.all(requests);
 };
 
+(async () => {
+  const results = await getArnieQuotes(['http://www.smokeballdev.com/arnie0', 'http://www.smokeballdev.com/arnie1', 'http://www.smokeballdev.com/arnie2']);
+  console.log(results);
+})();
 
-getArnieQuotes(['http://www.smokeballdev.com/arnie0', 'http://www.smokeballdev.com/arnie1', 'http://www.smokeballdev.com/arnie2']);
 module.exports = {
   getArnieQuotes,
 };
