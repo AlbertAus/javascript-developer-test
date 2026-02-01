e34c6b588aabba0d7eb054eb9d30ff629dc9fb29
AlbertAus <tonycdl80@gmail.com>
2026-02-01 16:21:23 +1100

    feat: finish up and add .history to .gitignore

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -4,14 +4,12 @@ const getArnieQuotes = async (urls) => {
   const requests = urls.map(async (url) => {
     const response = await httpGet(url);
     const { message } = JSON.parse(response.body);
-    
     if (response.status === 200) {
       return { 'Arnie Quote': message };
     } else {
       return { 'FAILURE': message };
     }
   });
-  
   return Promise.all(requests);
 };
 
