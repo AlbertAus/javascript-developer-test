a3e672e57d9b30864c7d483b62d90db310722512
AlbertAus <tonycdl80@gmail.com>
2026-02-01 16:17:48 +1100

    feat: remove console.log and testing code

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,7 +3,6 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   const requests = urls.map(async (url) => {
     const response = await httpGet(url);
-     console.log('Response:', response);
     const { message } = JSON.parse(response.body);
     
     if (response.status === 200) {
@@ -15,11 +14,6 @@ const getArnieQuotes = async (urls) => {
   
   return Promise.all(requests);
 };
-
-(async () => {
-  const results = await getArnieQuotes(['http://www.smokeballdev.com/arnie0', 'http://www.smokeballdev.com/arnie1', 'http://www.smokeballdev.com/arnie2']);
-  console.log(results);
-})();
 
 module.exports = {
   getArnieQuotes,
